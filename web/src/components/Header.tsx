50f3dd97130bf350febc630df09cb8a9887fc2d2
isaque.s <isaque.error404@gmail.com>
2023-01-18 17:11:26 -0300

    feat: create new component Header and add logo and styles

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Plus } from "phosphor-react";
+import logo from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+      <img src={logo} alt="logo de habits" />
+
+      <button
+        type="button"
+        className="border border-violet-500 font-semibold py-4 px-6 rounded-lg flex items-center gap-3 hover:border-violet-400"
+      >
+        <Plus size={20} className="text-violet-500" />
+        Novo hábito
+      </button>
+    </div>
+  );
+}
