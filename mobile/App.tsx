42394e1f5c8fcfc18f3614c00a76401817dc83f7
isaque.s <isaque.error404@gmail.com>
2023-01-18 19:02:09 -0300

    feat: add component Home and removed styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,8 +6,9 @@ import {
   useFonts,
 } from "@expo-google-fonts/inter";
 
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -22,30 +23,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  text: {
-    color: "#fff",
-    fontFamily: "Inter_800ExtraBold,",
-  },
-});
