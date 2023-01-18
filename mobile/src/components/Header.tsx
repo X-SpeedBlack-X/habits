781634272039394acf7a0f9627a78e6535a63ba7
isaque.s <isaque.error404@gmail.com>
2023-01-18 19:00:34 -0300

    feat: create new component Header and add styles

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Feather } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
+import Logo from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
+
+      <TouchableOpacity
+        activeOpacity={0.4}
+        className="flex-row items-center border border-violet-500 px-4 h-11 rounded-lg"
+      >
+        <Feather name="plus" size={20} color={colors.violet[500]} />
+        <Text className="text-white font-semibold text-base ml-3">
+          Novo hábito
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
