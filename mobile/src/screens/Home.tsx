d28dc9764c2f6f94d1e8b98b0c10234f54c591a1
isaque.s <isaque.error404@gmail.com>
2023-01-18 19:29:45 -0300

    feat: add utils, scrips and ScrollView

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,13 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const minimumSummaryDatesSizes = 18 * 5;
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -22,7 +27,27 @@ export function Home() {
           );
         })}
       </View>
-      <HabitDay />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({
+              length: amountOfDaysToFill,
+            }).map((_, i) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 border-zinc-800 m-1 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
