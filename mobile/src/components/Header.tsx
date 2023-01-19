2b18185f3060ca5fd34eb24c359ff80a2e3a1a11
isaque.s <isaque.error404@gmail.com>
2023-01-19 15:57:59 -0300

    feat: used navigate

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
+
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -11,6 +15,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.4}
         className="flex-row items-center border border-violet-500 px-4 h-11 rounded-lg"
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" size={20} color={colors.violet[500]} />
         <Text className="text-white font-semibold text-base ml-3">
